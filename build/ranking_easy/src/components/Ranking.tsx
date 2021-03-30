58fcb8c07ef0f75228b8c3caa4e2fc20174b0cff
hk-p <hukada.gogotea@gmail.com>
2021-03-30 20:17:43 +0900

    ランキングをスコア順に並べかえ成功

--- build/ranking_easy/src/components/Ranking.tsx
+++ build/ranking_easy/src/components/Ranking.tsx
@@ -1,44 +1,33 @@
-import { FC, useState, useEffect } from "react";
-import UserCard from "./UserCard";
-import { db } from "../config.rnk";
+import { FC, useState, useEffect } from 'react';
+import UserCard from './UserCard';
+import { db } from '../config.rnk';
 // import "./Ranking.css";
 
 const Ranking: FC = () => {
-	const [data, setData] = useState([{ name: "初期値", score: 404 }]);
-	const [sortedData, setSortedData] = useState([
-		{ name: "初期値", score: 999 },
-	]);
+	const [data, setData] = useState([{ name: '', score: 0 }]);
 
 	useEffect(() => {
 		// @ts-ignore
-		const unSub = db.collection("profiles").onSnapshot((snapshot) => {
-			setData(
-				// @ts-ignore
-				snapshot.docs.map((doc) => ({
-					name: doc.data().name,
-					score: doc.data().score,
-				}))
-			);
-			console.log(data);
-		});
-		data.sort((a, b) => b.score - a.score);
-		console.log("ソート1", data);
-
+		const unSub = db
+			.collection('profiles')
+			.orderBy('score', 'desc')
+			.onSnapshot((snapshot) => {
+				setData(
+					snapshot.docs.map((doc) => ({
+						name: doc.data().name,
+						score: doc.data().score,
+					}))
+				);
+				console.log('データ取得', data);
+			});
 		return () => unSub();
-		// eslint-disable-next-line
-	}, []);
-
-	useEffect(() => {
-		const newData = data;
-		setSortedData(newData);
-		console.log("そーと2", newData);
 		// eslint-disable-next-line
 	}, []);
 
 	return (
 		<>
 			<div className="user-list">
-				{sortedData.map((doc, i) => (
+				{data.map((doc, i) => (
 					<UserCard
 						className="user-card"
 						key={i}
